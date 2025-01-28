c651b9662adc8d12b18850fec1f7435563671ad0
Rahul Kumar Yadav <124893197+kyrahul2107@users.noreply.github.com>
2025-01-28 16:12:38 +0530

    Tile Position Corrected

--- SnakeGame/assets/scripts/Snake.js
+++ SnakeGame/assets/scripts/Snake.js
@@ -1,69 +1,105 @@
 cc.Class({
   extends: cc.Component,
+
   properties: {
     bodyPrefab: cc.Prefab,
     foodPrefab: cc.Prefab,
     scoreNode: cc.Label,
     timerLabel: cc.Label,
     delayTime: 5,
-    positionsArray: [],
+    snakeBody: [],
   },
 
   onLoad() {
     cc.director.getCollisionManager().enabled = true;
-    this.direction = cc.v2(1, 0);
+    cc.director.getCollisionManager().enabledDebugDraw = true;
     this.snakeBody = [];
+
+    // Create and position the head of the snake
     const head = cc.instantiate(this.bodyPrefab);
     head.parent = this.node.parent;
     this.snakeBody.push(head);
-    this.positionsArray.push(
-      new cc.v2(this.snakeBody[0].x, this.snakeBody[0].y)
-    );
+
+    // Spawn initial food
     this.spawnFood();
+
+    this.schedule(this.moveSnake, 0.4);
+
+    // Initialize score and timer
     this.score = 0;
     this.updateScore();
-    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-    this.checkBoundaryCollision();
     this.currentTime = this.delayTime;
     this.schedule(this.updateTimerLabel, 1);
+
+    // Schedule game over and score save
     this.scheduleOnce(() => {
       this.loadEndGameScene();
       this.saveScore();
     }, this.delayTime);
+
+    // Setup input listeners
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+
+    // Load score history
     this.scoreHistory =
       JSON.parse(cc.sys.localStorage.getItem("scoreHistory")) || [];
   },
 
+  onKeyDown(event) {
+    const headComp = this.snakeBody[0].getComponent("SnakeElement");
 
-  saveScore() {
-    // Add the current score to the history
-    console.log("Save Score Method is Called");
-    const newScore = {
-      score: this.score, // Use this.score to assign the current score
-      date: new Date().toLocaleDateString(),
-      time: new Date().toLocaleTimeString(),
-    };
-    // Push the newScore object to the scoreHistory array
-    this.scoreHistory.push(newScore);
-    // Save the updated history to local storage
-    cc.sys.localStorage.setItem(
-      "scoreHistory",
-      JSON.stringify(this.scoreHistory)
-    );
+    switch (event.keyCode) {
+      case cc.macro.KEY.left:
+        if (headComp.direction.x !== 1) headComp.direction = cc.v2(-1, 0); 
+        break;
+      case cc.macro.KEY.right:
+        if (headComp.direction.x !== -1) headComp.direction = cc.v2(1, 0); 
+        break;
+      case cc.macro.KEY.up:
+        if (headComp.direction.y !== -1) headComp.direction = cc.v2(0, 1);
+        break;
+      case cc.macro.KEY.down:
+        if (headComp.direction.y !== 1) headComp.direction = cc.v2(0, -1);
+        break;
+    }
   },
 
-  checkCollision(nodeA, nodeB) {
-    // Get bounding boxes of both nodes
-    const boxA = nodeA.getBoundingBoxToWorld();
-    const boxB = nodeB.getBoundingBoxToWorld();
+  growSnake() {
+    const newSegment = cc.instantiate(this.bodyPrefab);
+    newSegment.parent = this.node.parent;
 
-    // Check for intersection
-    return boxA.intersects(boxB);
+    const lastSegment = this.snakeBody[this.snakeBody.length - 1];
+    const lastSegmentComp = lastSegment.getComponent("SnakeElement");
+    const newSegmentPos = lastSegmentComp.calculateNewSegmentPosition();
+
+    newSegment.setPosition(newSegmentPos);
+    this.snakeBody.push(newSegment);
+  },
+
+  moveSnake() {
+    this.checkFoodCollision();
+    this.checkSelfCollision();
+
+    const previousPositions = [];
+
+    const head = this.snakeBody[0];
+    const headComp = head.getComponent("SnakeElement");
+    previousPositions.push(head.position);
+
+    headComp.move();
+
+    for (let i = 1; i < this.snakeBody.length; i++) {
+      const currentSegment = this.snakeBody[i];
+
+      previousPositions[i] = currentSegment.position;
+
+      currentSegment.setPosition(previousPositions[i - 1]);
+    }
   },
 
   spawnFood() {
     const parentNode = this.node.parent;
-    const tileSize = 40; // Tile size
+    const tileSize = 40; 
     const halfWidth = parentNode.width / 2;
     const halfHeight = parentNode.height / 2;
     const columns = Math.floor(parentNode.width / tileSize);
@@ -72,49 +108,71 @@ cc.Class({
     const randomRow = Math.floor(Math.random() * rows);
     const foodPosX = randomCol * tileSize - halfWidth + tileSize / 2;
     const foodPosY = randomRow * tileSize - halfHeight + tileSize / 2;
+
     this.foodNode = cc.instantiate(this.foodPrefab);
     this.foodNode.setPosition(cc.v2(foodPosX, foodPosY));
     parentNode.addChild(this.foodNode);
   },
-  
 
+  checkCollision(nodeA, nodeB) {
+    const boxCollider = nodeA.getComponent(cc.PhysicsBoxCollider);
+    const circleCollider = nodeB.getComponent(cc.PhysicsCircleCollider);
   
-  foodCollisionOccu() {
-    console.log("Collision Occured");
-    this.foodNode.destroy();
-    this.spawnFood();
-    this.growSnake2();
-    this.incrementScore();
+    const boxWorldPos = nodeA.convertToWorldSpaceAR(cc.v2(0, 0)); // Box position in world space
+    const boxWidth = boxCollider.size.width;
+    const boxHeight = boxCollider.size.height;
+
+    // Get the circle collider's world position and radius
+    const circleWorldPos = nodeB.convertToWorldSpaceAR(cc.v2(0, 0)); // Circle position in world space
+    const circleRadius = circleCollider.radius;
+
+    // Find the closest point on the box to the circle center
+    const closestX = Math.max(
+      boxWorldPos.x - boxWidth / 2,
+      Math.min(circleWorldPos.x, boxWorldPos.x + boxWidth / 2)
+    );
+    const closestY = Math.max(
+      boxWorldPos.y - boxHeight / 2,
+      Math.min(circleWorldPos.y, boxWorldPos.y + boxHeight / 2)
+    );
+
+    // Calculate the distance between the closest point and the circle's center
+    const distanceX = circleWorldPos.x - closestX;
+    const distanceY = circleWorldPos.y - closestY;
+    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+    // If the distance is less than or equal to the circle's radius, it's a collision
+    return distance <= circleRadius;
   },
 
-  growSnake2() {
-    const newSegment = cc.instantiate(this.bodyPrefab);
-    newSegment.parent = this.node.parent;
-  
-    // Get the tail position
-    const lastSegmentPos = this.positionsArray[this.positionsArray.length - 1];
-  
-    // Calculate offset based on direction
-    let offset = cc.v2(0, 0); // Default no offset
-    if (this.direction.x !== 0) {
-      // Moving horizontally: offset along X-axis
-      offset = cc.v2(-this.direction.x * newSegment.width, 0);
-    } else if (this.direction.y !== 0) {
-      // Moving vertically: offset along Y-axis
-      offset = cc.v2(0, -this.direction.y * newSegment.height);
+  checkSelfCollision() {
+    const head = this.snakeBody[0];
+    for (let i = 1; i < this.snakeBody.length; i++) {
+      const segment = this.snakeBody[i];
+      if (this.arePositionsEqual(head.position, segment.position)) {
+        console.log("Game Over!");
+        cc.director.loadScene("HomeScreen");
+        return true;
+      }
     }
-  
-    // Calculate new position with offset
-    const newSegmentPos = lastSegmentPos.add(offset);
-  
-    // Add the new position to the array
-    this.positionsArray.push(newSegmentPos);
-    this.snakeBody.push(newSegment);
-  
-    // Position the new segment
-    newSegment.setPosition(newSegmentPos);
+
+    return false;
   },
-  
+
+  arePositionsEqual(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y;
+  },
+
+  checkFoodCollision() {
+    const head = this.snakeBody[0];
+    if (this.checkCollision(head, this.foodNode)) {
+      this.foodNode.destroy();
+      this.growSnake();
+      console.log("Food Collision Occured");
+      this.spawnFood();
+      this.incrementScore();
+    }
+  },
 
   incrementScore() {
     this.score += 10;
@@ -123,24 +181,7 @@ cc.Class({
 
   updateScore() {
     if (this.scoreNode) {
-      this.scoreNode.string = `Score:${this.score}`;
-    }
-  },
-
-  checkBoundaryCollision() {
-    console.log(`Boundary Collision Occured`);
-    const parent = this.node.parent;
-    const halfWidth = parent.width / 2;
-    const halfHeight = parent.height / 2;
-    const head = this.snakeBody[0];
-
-    if (
-      head.x < -halfWidth ||
-      head.x > halfWidth ||
-      head.y < -halfHeight ||
-      head.y > halfHeight
-    ) {
-      this.endGame();
+      this.scoreNode.string = `Score: ${this.score}`;
     }
   },
 
@@ -152,10 +193,27 @@ cc.Class({
     }
   },
 
+  saveScore() {
+    console.log("Save Score Method is Called");
+
+    const newScore = {
+      score: this.score,
+      date: new Date().toLocaleDateString(),
+      time: new Date().toLocaleTimeString(),
+    };
+
+    this.scoreHistory.push(newScore);
+    cc.sys.localStorage.setItem(
+      "scoreHistory",
+      JSON.stringify(this.scoreHistory)
+    );
+  },
+
   loadEndGameScene() {
     console.log("Game Over!");
     cc.director.loadScene("HomeScreen");
   },
+
   onDestroy() {
     cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
   },
--- SnakeGame/assets/scripts/SnakeElement.js
+++ SnakeGame/assets/scripts/SnakeElement.js
@@ -8,51 +8,36 @@ cc.Class({
 
   onLoad() {
     this.direction = cc.v2(1, 0);
-    this.position = cc.v2(0, 0);
-    console.log("OnLoad Method Called");
   },
 
-  start() {
-    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-    console.log("Start Method is called");
-    this.schedule(this.moveSnake, this.moveSpeed);
-  },
-
-  onKeyDown(event) {
-    switch (event.keyCode) {
-      case cc.macro.KEY.left:
-        if (this.direction.x !== 1) this.direction = cc.v2(-1, 0); // Move left
-        break;
-      case cc.macro.KEY.right:
-        if (this.direction.x !== -1) this.direction = cc.v2(1, 0); // Move right
-        break;
-      case cc.macro.KEY.up:
-        if (this.direction.y !== -1) this.direction = cc.v2(0, 1); // Move up
-        break;
-      case cc.macro.KEY.down:
-        if (this.direction.y !== 1) this.direction = cc.v2(0, -1); // Move down
-        break;
-    }
-  },
-
-  moveSnake() {
-    let newPosition = cc.v2(
+  move() {
+    const newPosition = cc.v2(
       this.node.x + this.direction.x * this.tileSize,
       this.node.y + this.direction.y * this.tileSize
     );
     if (this.isPositionWithinBounds(newPosition)) {
-      console.log("New position is within bounds:", newPosition);
       this.node.setPosition(newPosition);
-      console.log("Moved to new position:", this.node.position);
     } else {
-      console.log("New position is out of bounds:", newPosition);
+      console.log("Boundary Collision Detected");
     }
+  },
+
+  calculateNewSegmentPosition() {
+    const currentPos = this.node.position;
+    const direction = this.lastDirection || cc.v2(0, -1); 
+
+    const offset = 40;
+    const newX = currentPos.x - direction.x * offset;
+    const newY = currentPos.y - direction.y * offset;
+
+    return cc.v2(newX, newY);
   },
 
   isPositionWithinBounds(position) {
     const parentNode = this.node.parent;
     const halfWidth = parentNode.width / 2;
     const halfHeight = parentNode.height / 2;
+
     return (
       position.x >= -halfWidth + this.tileSize / 2 &&
       position.x <= halfWidth - this.tileSize / 2 &&
